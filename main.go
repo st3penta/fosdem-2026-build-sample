dd119a242ee0370694a4979414054cef8c5c4e08
Stefano Pentassuglia <spentass@redhat.com>
2026-01-19 14:45:16 +0100

    add sample dependency

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"github.com/rs/zerolog/log"
+)
 
 func main() {
-	fmt.Println("Hello, World! I'm a build sample")
+	log.Print("Hello, World! I'm a build sample")
 }
